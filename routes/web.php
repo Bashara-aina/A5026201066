8d713b9e86cb260d75726a8e84403bc47182eea3
Bashara-aina <Bashara.aina.56@gmail.com>
2021-11-24 23:24:01 +0700

    Kuliah 18 November 2021

--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,24 @@ Route::get('ets2021',"Tugas_Controller@showETS");
 Route::get('tugasphp',"Tugas_Controller@tugas_php") ;
 Route::post('tugasphp',"Tugas_Controller@tugas_php") ;
 
+//route CRUD
+Route::get('/pegawai','PegawaiController@index');
 
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+
+Route::post('/pegawai/store','PegawaiController@store');
+
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+
+Route::post('/pegawai/update','PegawaiController@update');
+
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
+
+//route CRUD
+Route::get('/pegawai','PegawaiController@index');
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+Route::post('/pegawai/store','PegawaiController@store');
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+Route::post('/pegawai/update','PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
 ?>
